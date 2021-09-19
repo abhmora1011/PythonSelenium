9779f57250c609611e52721f3d78a1dd9177da19
abhmora1011 <abraham.morial.ora@gmail.com>
2021-09-19 15:42:07 +0800

    Added new pyton file that contains the script for handling a dynamic checkbox and radio button

---
+++ PythonSelenium/SeleniumCheckboxDynamic.py
@@ -0,0 +1,29 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver = webdriver.Chrome(executable_path="/Users/hnstabe/Documents/chromedriver")
+
+driver.get("https://www.rahulshettyacademy.com/AutomationPractice/")
+
+driver.maximize_window()
+
+checkBoxes = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
+
+print(len(checkBoxes))
+
+for checkBox in checkBoxes:
+    if checkBox.get_attribute("value") == "option2":
+        checkBox.click()
+        assert checkBox.is_selected()
+
+
+radioBtn = driver.find_elements(By.NAME, "radioButton")
+
+radioBtn[2].click()
+
+assert radioBtn[2].is_selected()
+
+
